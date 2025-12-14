de0a1e72d9a0636cbb56a2a8f0253e6a49877f4f
Sevilze <sevilzcubing@gmail.com>
2025-12-15 02:10:49 +0700

    feat: wire tauri app to the rust solver.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,16 @@
+mod commands;
+
+use commands::SolverHandle;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .setup(|app| {
-            if cfg!(debug_assertions) {
-                app.handle().plugin(
-                    tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
-                        .build(),
-                )?;
-            }
-            Ok(())
-        })
+        .manage(SolverHandle::default())
+        .plugin(tauri_plugin_log::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![
+            commands::solve,
+            commands::cancel_solve
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
