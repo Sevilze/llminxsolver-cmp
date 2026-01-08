48c55ddc789fd04215a88dea18754b761ab9286d
Sevilze <sevilzcubing@gmail.com>
2026-01-08 23:09:36 +0700

    initialize flakes file and add caching to the workflows build.

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "llminxsolver - Megaminx puzzle solver with Compose Multiplatform UI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "llminxsolver-uniffi";
+          version = "0.1.0";
+
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            openssl
+          ];
+
+          cargoBuildFlags = [ "-p" "llminxsolver-uniffi" ];
+
+          meta = with pkgs.lib; {
+            description = "Megaminx puzzle solver with UniFFI bindings";
+            homepage = "https://github.com/Sevilze/llminxsolver-cmp";
+            license = licenses.mit;
+            maintainers = [];
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+            openssl
+            jdk21
+          ];
+
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+        };
+      }
+    );
+}
